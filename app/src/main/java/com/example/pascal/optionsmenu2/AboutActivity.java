a4e3b0f8d9d89d64611f5956cb509f16912bc96a
papostolidis <40176652+papostolidis@users.noreply.github.com>
2018-07-04 16:02:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/pascal/optionsmenu2/AboutActivity.java
@@ -0,0 +1,62 @@
+package com.example.pascal.optionsmenu2;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_about, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.Main) {
+            startActivity(new Intent(AboutActivity.this, MainActivity.class));
+
+        } else if (id == R.id.Settings) {
+            startActivity(new Intent(AboutActivity.this, SettingsActivity.class));
+
+        } else if (id == R.id.Help) {
+            startActivity(new Intent(AboutActivity.this, HelpActivity.class));
+        }
+
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+}
